da48bc628384b3f9d92768986e27043875a69e98
felixzh <felixzh2020@126.com>
2021-07-13 19:30:07 +0800

    更新KafkaFlinkIdeaDemo到Flink1.12.2

--- KafkaFlinkIdeaDemo/src/main/java/KafkaFlinkIdea.java
+++ KafkaFlinkIdeaDemo/src/main/java/KafkaFlinkIdea.java
@@ -2,7 +2,8 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
 
 import java.util.Properties;
 
@@ -12,22 +13,21 @@ public class KafkaFlinkIdea {
         final ParameterTool para = ParameterTool.fromArgs(args);
         if (!para.has("path")) {
             System.out.println("Error: not exist --path /opt/your.properties");
-            System.out.println("Usage: flink run -m yarn-cluster -d /opt/your.jar --path /opt/your.properties");
+            System.out.println("Usage: flink run -t yarn-per-job -d /opt/your.jar --path /opt/your.properties");
             System.exit(0);
         }
 
-        if (OperatingSystem.IS_WINDOWS) {
+        ParameterTool param = ParameterTool.fromPropertiesFile(para.getRequired("path"));
+
+        if (param.getRequired("kerberos.enable").equalsIgnoreCase("true") && OperatingSystem.IS_WINDOWS) {
             System.setProperty("java.security.auth.login.config", "d:\\KafkaFlinkIdeaDemoConf//kafka_client_jaas.conf");
             System.setProperty("java.security.krb5.conf", "d:\\KafkaFlinkIdeaDemoConf//krb5.conf");
         }
 
-
-        ParameterTool param = ParameterTool.fromPropertiesFile(para.getRequired("path"));
-
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         if (param.getRequired("checkpoint.enable").equals("1")) {
             env.getConfig().setUseSnapshotCompression(true);
-            env.enableCheckpointing(Long.valueOf(param.getRequired("checkpoint.interval"))); // create a checkpoint every 5 seconds
+            env.enableCheckpointing(Long.parseLong(param.getRequired("checkpoint.interval"))); // create a checkpoint
         }
 
         env.getConfig().setGlobalJobParameters(param); // make parameters available in the web interface
@@ -35,11 +35,13 @@ public class KafkaFlinkIdea {
         Properties consumerProp = new Properties();
         consumerProp.setProperty("bootstrap.servers", param.getRequired("consumer.bootstrap.servers"));
         consumerProp.setProperty("group.id", param.getRequired("consumer.group.id"));
-        consumerProp.setProperty("security.protocol", "SASL_PLAINTEXT");
-        consumerProp.setProperty("sasl.mechanism", "GSSAPI");
-        consumerProp.setProperty("sasl.kerberos.service.name", "kafka");
+        if (param.getRequired("kerberos.enable").equalsIgnoreCase("true")) {
+            consumerProp.setProperty("security.protocol", "SASL_PLAINTEXT");
+            consumerProp.setProperty("sasl.mechanism", "GSSAPI");
+            consumerProp.setProperty("sasl.kerberos.service.name", "kafka");
+        }
 
-        FlinkKafkaConsumer011<String> kafkaConsumer = new FlinkKafkaConsumer011<>(param.getRequired("consumer.kafka.topic"),
+        FlinkKafkaConsumer<String> kafkaConsumer = new FlinkKafkaConsumer<>(param.getRequired("consumer.kafka.topic"),
                 new SimpleStringSchema(), consumerProp);
 
         DataStream<String> sourceDataStream = env.addSource(kafkaConsumer);
