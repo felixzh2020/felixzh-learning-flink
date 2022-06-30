f8f3a22a5e95a3d8907002244e7339356187b3d2
felixzh <felixzh2020@126.com>
2022-06-30 17:35:55 +0800

    udf

--- FlinkIdeaDemo/src/main/java/NCFlinkSqlUDFIdea.java
+++ FlinkIdeaDemo/src/main/java/NCFlinkSqlUDFIdea.java
@@ -3,7 +3,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.table.functions.ScalarFunction;
 
-public class NCFlinkSqlIdea {
+public class NCFlinkSqlUDFIdea {
     public static void main(String... args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
--- FlinkIdeaDemo/src/main/java/NCFlinkUDFIdea.java
+++ FlinkIdeaDemo/src/main/java/NCFlinkUDFIdea.java
@@ -2,7 +2,7 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-public class NCFlinkIdea {
+public class NCFlinkUDFIdea {
     public static void main(String... args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
